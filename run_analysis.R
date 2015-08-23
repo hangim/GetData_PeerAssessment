4c0cca9b0557340f615246dedf4fbbcd166f6b4c
hang <git@hang.im>
2015-08-23 10:02:08 +0800

    framework is ok

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,7 @@
-# step 4: Appropriately labels the data set with descriptive variable names. 
+library(data.table)
+
+# step 4:
+# Appropriately labels the data set with descriptive variable names. 
 a <- read.table("./UCI HAR Dataset/features.txt", stringsAsFactors = F)[, 2]
 
 
@@ -18,17 +21,20 @@ subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt",
             stringsAsFactor = FALSE, col.names = "subject")
 
 
-# step 1: Merges the training and the test sets to create one data set.
+# step 1:
+# Merges the training and the test sets to create one data set.
 y <- cbind(rbind(X_train, X_test),
            rbind(y_train, y_test),
            rbind(subject_train, subject_test))
 
 
-# step 3: Uses descriptive activity names to name the activities in the data set
+# step 3:
+# Uses descriptive activity names to name the activities in the data set
 y$active <- factor(y$active, labels = c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING"))
 
 
-# step 2: Extracts only the measurements on the mean and standard deviation for each measurement. 
+# step 2:
+# Extracts only the measurements on the mean and standard deviation for each measurement. 
 #k <- c(1:6, 41:46, 81:86, 121:126, 161:166, 201:202, 214:215, 227:228, 240, 241, 253:254, 266:271, 294:296, 345:350, 373:375, 424:429, 452:454, 503:504, 513, 516:517, 526, 529:530, 539, 542:543, 552)
 #yy = y[, k]
 
@@ -36,7 +42,9 @@ y$active <- factor(y$active, labels = c("WALKING", "WALKING_UPSTAIRS", "WALKING_
 yy = y[, 1:6]
 
 
-# step 5: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-dt <- data.table(y)
-dt[, mean(tBodyAcc.mean...X), by = list(active, subject)]
-
+# step 5:
+# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+tidy_data <- aggregate(y[, 1:561],
+                       by = list(active = y$active, subject = y$subject),
+                       mean)
+write.table(a, "tidy.txt", row.name = FALSE)
